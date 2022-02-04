84e0600464d2e0e54eb59f64dcfe4ea018295d38
BT_1E10_27 <wrwgwgh>
2022-02-04 11:47:53 -0500

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class main
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> numList = new ArrayList<Integer>();
+
+        numList.add(3);
+        numList.add(0, 6);
+        numList.add(2, 7);
+        numList.add(2, 8);
+        numList.set(3, 9);
+        numList.add(1, 2);
+        numList.add(1, numList.size());
+        numList.remove(4);
+        numList.set(numList.size()-1, 0);
+        System.out.println(numList);
+
+    }
+}
+
+
+
